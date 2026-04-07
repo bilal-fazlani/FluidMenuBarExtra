839c67fa154b0cb0ca4796763f538ddce250e54b
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2026-04-07 12:07:23 +0530

    feat: add updateImage method to FluidMenuBarExtraStatusItem

--- Sources/FluidMenuBarExtra/FluidMenuBarExtraStatusItem.swift
+++ Sources/FluidMenuBarExtra/FluidMenuBarExtraStatusItem.swift
@@ -156,6 +156,10 @@ final class FluidMenuBarExtraStatusItem: NSObject, NSWindowDelegate {
         }
     }
 
+    func updateImage(_ image: NSImage) {
+        statusItem.button?.image = image
+    }
+
     func windowDidBecomeKey(_ notification: Notification) {
         globalEventMonitor?.start()
         setButtonHighlighted(to: true)
